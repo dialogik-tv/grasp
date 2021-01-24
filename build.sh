ebc283b3f6db3ab29a659753d5edd2f5965833f7
Moritz <moritz@dialogik.tv>
2021-01-24 21:03:22 +0100

    Add build script

--- build.sh
+++ build.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+# Build dist
+npm run build
+
 # Remove old data
 rm -rf ../grasp-dist/css
 rm -rf ../grasp-dist/js
 rm ../grasp-dist/index.html
 
 # Move dist folder to dist directory
-mv dist/* ../grasp-dist/
+cp -r dist/* ../grasp-dist/
