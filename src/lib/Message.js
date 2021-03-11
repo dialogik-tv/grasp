326564c68f4a5f04222f7be323291a843d5a33c3
dialogik <dialogik@users.noreply.github.com>
2021-03-11 21:40:26 +0100

    Add missing timestamp to Message

--- src/lib/Message.js
+++ src/lib/Message.js
@@ -3,8 +3,9 @@ class Message {
         // Remove any HTML tags – and skip message if there is no content left
         this.message = this.sanitizeMessage(input.message);
         
-        // Add username, badges and emotes
+        // Add required attributes
         this.username = input.tags.displayName;
+        this.timestamp = input.timestamp;
         this.badges = input.tags.badges;
         this.emotes = input.tags.emotes;
 
